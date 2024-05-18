7de291fb9a2358892363c403b9ed198ee1fce215
Tayyib Polat <tayyibpolat@gmx.de>
2024-05-18 14:23:12 +0200

    Finally done with it exept the hover effect

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-let container = document.getElementById("container");
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('container');
+    const totalItems = 50 * 50;
 
-let btn = document.createElement("button");
-document.body.appendChild(btn);
-btn.textContent = "button";
-
-let input = prompt("n enter an odd number between 1 and 99:");
-let number = parseInt(input);
-
-for (let i = 0; i < number; i++) {
-    let div = document.createElement("div");
-    div.classList.add("item");
-    container.appendChild(div);
-}
+    for (let i = 0; i < totalItems; i++) {
+        const item = document.createElement('div');
+        item.className = 'grid-item';
+        container.appendChild(item);
+    }
+});
