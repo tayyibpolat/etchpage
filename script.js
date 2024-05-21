6789b611ef93d06ec14fb773fb3eacd69af4c6dc
Tayyib Polat <tayyibpolat@gmx.de>
2024-05-21 16:54:02 +0200

    move the btn top and contai to bottom

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('container');
-    const totalItems = 50 * 50;
 
-    for (let i = 0; i < totalItems; i++) {
+    let btn = document.createElement("button");
+    btn.textContent = "Button";
+    document.body.appendChild(btn);
+    document.body.insertBefore(btn, container);
+
+    let input = prompt("choose a number between 0 and 100");
+    let num = input * input;
+    for (let i = 0; i < num; i++) {
         const item = document.createElement('div');
         item.className = 'grid-item';
         container.appendChild(item);
