72647a299f2ac5fb9bd872e5c364a5e65207407e
Tayyib Polat <tayyibpolat@gmx.de>
2024-05-21 17:03:14 +0200

    Click button to get a prompt instead of reloading

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
     btn.textContent = "Button";
     document.body.appendChild(btn);
     document.body.insertBefore(btn, container);
-
-    let input = prompt("choose a number between 0 and 100");
-    let num = input * input;
-    for (let i = 0; i < num; i++) {
-        const item = document.createElement('div');
-        item.className = 'grid-item';
-        container.appendChild(item);
-    }
+    btn.addEventListener('click', function() {
+        let input = prompt("choose a number between 0 and 100");
+        let num = input * input;
+        for (let i = 0; i < num; i++) {
+           const item = document.createElement('div');
+           item.className = 'grid-item';
+           container.appendChild(item);
+        };
+    });
 });
